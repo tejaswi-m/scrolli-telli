b0140e7b6bd3a4f7169049680b4391e353c930d9
manchite <manchite@gvsu.edu>
2026-01-15 14:01:05 -0500

    adding a base name

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/scrolli-telli">
       <App />
     </BrowserRouter>
   </React.StrictMode>
